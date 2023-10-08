49017294691e7c1b4af178e845a8a679540173d0
Michael Salerno <msalerno1993@gmail.com>
2023-10-08 13:13:29 -0400

    complete superquality section of application

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,32 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red">Quality</span> Shoes
+          <br />
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          We ensure premium comfort and style with our meticulously crafted
+          footwear that is designed to elevate your experience.
+        </p>
+        <div className="mt-11 ">
+        <Button label="View Details"/>
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" />
+      </div>
+    </section>
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
